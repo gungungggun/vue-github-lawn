57e0dda6943cdc79e28fca51e0a4702afb3a966e
gun <gungungggun@gmail.com>
2017-08-12 09:40:28 +0900

    first

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <github-lawn></github-lawn>
+  </div>
+</template>
+
+<script>
+import GithubLawn from './components/GithubLawn'
+
+export default {
+  name: 'app',
+  components: {
+    GithubLawn
+  }
+}
+</script>
