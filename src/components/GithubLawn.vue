b0a859efa437192424312707531cca6cd9912561
gun <gungungggun@gmail.com>
2017-08-12 15:14:37 +0900

    ロジック追加

--- src/components/GithubLawn.vue
+++ src/components/GithubLawn.vue
@@ -1,22 +1,8 @@
 <template>
   <div class="github-lawn">
-    <ul>
-      <li class="l1"></li>
-      <li class="l3"></li>
-      <li class="l3"></li>
-      <li class="l1"></li>
-      <li class="l4"></li>
-      <li class="l5"></li>
-      <li class="l1"></li>
-    </ul>
-    <ul>
-      <li class="l2"></li>
-      <li class="l4"></li>
-      <li class="l1"></li>
-      <li class="l2"></li>
-      <li class="l5"></li>
-      <li class="l3"></li>
-      <li class="l1"></li>
+    <p>{{ sum }}</p>
+    <ul v-for="week in github">
+      <li v-for="d in week" :class="calc(d)"></li>
     </ul>
   </div>
 </template>
@@ -26,7 +12,86 @@ export default {
   name: 'github-lawn',
   data () {
     return {
-      msg: 'first'
+      data: [
+        1, 0, 6, 1, 8, 0, 4, 0, 3, 11, 5, 10, 1, 2
+      ]
+    }
+  },
+  computed: {
+    github () {
+      let count = 0
+      let data = []
+      let week = []
+      this.data.forEach(d => {
+        if (count % 7 === 0) {
+          week = []
+        }
+        week.push(d)
+        if (count % 7 === 6) {
+          data.push(week)
+        }
+        count++
+      })
+      console.log(data)
+      return data
+    },
+    max () {
+      return Math.max.apply(null, this.data)
+    },
+    min () {
+      return Math.min.apply(null, this.data)
+    },
+    sum () {
+      let sum = 0
+      let ii = this.data.length
+      for (let i = 0; i < ii; i++) {
+        sum += this.data[i]
+      }
+      return sum
+    },
+    avg () {
+      return this.sum / this.data.length
+    },
+    median () {
+      let copy = this.data.concat()
+      let sort = copy.filter(function (x) {
+        return (x > 0)
+      })
+      sort.sort(function (a, b) {
+        if (a < b) {
+          return -1
+        }
+        if (a > b) {
+          return 1
+        }
+        return 0
+      })
+      console.log('m')
+      if (sort.length % 2 === 0) {
+        let m1 = sort.length / 2
+        let m2 = m1 - 1
+        return (sort[m1] + sort[m2]) / 2
+      } else {
+        return sort[(sort.length - 1) / 2]
+      }
+    }
+  },
+  methods: {
+    calc (d) {
+      if (d > 0) {
+        if (d > this.median) {
+          if (d > (this.median + this.max) / 2) {
+            return 'l5'
+          }
+          return 'l4'
+        } else {
+          if (d > (this.median / 2)) {
+            return 'l3'
+          }
+          return 'l2'
+        }
+      }
+      return 'l1'
     }
   }
 }
@@ -34,18 +99,19 @@ export default {
 
 <style scoped>
 .github-lawn {
+  clear:both;
 }
 .github-lawn ul {
   list-style:none;
-  display:inline-block;
   margin:0;
   padding:0;
+  float:left;
 }
 .github-lawn ul li {
   width:10px;
   height:10px;
   background:#f0f0f0;
-  margin-bottom:1px;
+  margin:1px;
 }
 .github-lawn ul li:last-child {
   margin-bottom:0;
