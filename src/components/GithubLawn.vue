73bc8bc319cb0449c96e1065b2c280825686defb
gun <gungungggun@gmail.com>
2017-08-12 10:20:03 +0900

    component作成中

--- src/components/GithubLawn.vue
+++ src/components/GithubLawn.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="github-lawn">
+    <ul>
+      <li class="l1"></li>
+      <li class="l3"></li>
+      <li class="l3"></li>
+      <li class="l1"></li>
+      <li class="l4"></li>
+      <li class="l5"></li>
+      <li class="l1"></li>
+    </ul>
+    <ul>
+      <li class="l2"></li>
+      <li class="l4"></li>
+      <li class="l1"></li>
+      <li class="l2"></li>
+      <li class="l5"></li>
+      <li class="l3"></li>
+      <li class="l1"></li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
+  name: 'github-lawn',
   data () {
     return {
       msg: 'first'
@@ -16,7 +33,36 @@ export default {
 </script>
 
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.github-lawn {
+}
+.github-lawn ul {
+  list-style:none;
+  display:inline-block;
+  margin:0;
+  padding:0;
+}
+.github-lawn ul li {
+  width:10px;
+  height:10px;
+  background:#f0f0f0;
+  margin-bottom:1px;
+}
+.github-lawn ul li:last-child {
+  margin-bottom:0;
+}
+.github-lawn ul li.l1 {
+  background:#f0f0f0;
+}
+.github-lawn ul li.l2 {
+  background:#c6e48b;
+}
+.github-lawn ul li.l3 {
+  background:#7bc96f;
+}
+.github-lawn ul li.l4 {
+  background:#239a3b;
+}
+.github-lawn ul li.l5 {
+  background:#196127;
 }
 </style>
